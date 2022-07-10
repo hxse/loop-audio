90c437eac9d0c481469151e7f953cc87b050d64f
hxse <narutohy@163.com>
2022-07-10 23:43:02 +0800

    push

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 var cacheName = "hello-pwa";
-var filesToCache = ["/", "/index.html", "/css/style.css", "/js/main.js"];
+var filesToCache = ["/", "/audio/"];
 
-/* Start the service worker and cache all of the app's content */
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
@@ -11,7 +10,6 @@ self.addEventListener("install", function (e) {
   self.skipWaiting();
 });
 
-/* Serve cached content when offline */
 self.addEventListener("fetch", function (e) {
   e.respondWith(
     caches.match(e.request).then(function (response) {
