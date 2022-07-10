bc025bdbab4e2e9ed6a6906ddd53004cd14c6c26
hxse <narutohy@163.com>
2022-07-10 23:30:13 +0800

    push

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 window.onload = () => {
-  'use strict';
+  "use strict";
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/loop-audio/sw.js");
   }
-}
+};
